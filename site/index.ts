e20ae255ded69e53bd438e3e347d752044119cff
mismorgano <mismorgano@gmail.com>
2025-10-30 03:09:04 -0600

    update: index.ts to bundle files (before 11ty)
    Accordantly update .gitignore to ignore output and
    index.html to be aware of the new path.

--- site/index.ts
+++ site/index.ts
@@ -1,1 +1,8 @@
 console.log("Hello via Bun!");
+
+// to bundle the first app
+await Bun.build({
+    entrypoints: ['../ArtificialIntelligence/Tareas/Tarea-01/src/puzzle.ts'],
+    outdir: '../ArtificialIntelligence/Tareas/Tarea-01/out',
+    format: "esm"
+})
