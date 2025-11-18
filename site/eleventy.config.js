d17db2a2543befca713bf4b61b21635006e576c8
mismorgano <mismorgano@gmail.com>
2025-11-17 21:25:49 -0600

    update: basic site routing for the assignments.
    Due to the update in the way it compiles and saves the pdfs,
    now I have to recreate that layout in the (html) site.
    
    So, in eleventy.config.js I add a collection that basically just scans the
    pdfs folder that will be previously created by the github action and the
    it simple returns an array of objects with properties {title, course, url}.
    
    Then those properties are used in the index html that 11ty then process
    with liquid in order to give a simple structure (a list) for the assignments.

--- site/eleventy.config.js
+++ site/eleventy.config.js
@@ -1,24 +1,50 @@
+import { glob } from 'fs/promises';
+import path from 'path';
 
-export default async function(eleventyConfig) {
-	
+export default async function (eleventyConfig) {
+
 	// Configure Eleventy
 
-	// to get a live server on my root directory
-	eleventyConfig.setInputDirectory("..");
-
-	// avoid including this site/ directory in the build
-	eleventyConfig.ignores.add(".")
-
 	// to just copy bundled files
-    eleventyConfig.addPassthroughCopy('../ArtificialIntelligence/Tareas/Tarea-01/out/');
+	eleventyConfig.addPassthroughCopy({ '../ArtificialIntelligence/Tareas/Tarea-01/out/': '/ArtificialIntelligence/Tareas/Tarea-01/out/' });
+	// to copy index.html from assignment
+	eleventyConfig.addPassthroughCopy({ '../ArtificialIntelligence/Tareas/Tarea-01/index.html': '/ArtificialIntelligence/Tareas/Tarea-01/' });
 
-    // eleventyConfig.addTemplateFormats("js");
-    // eleventyConfig.addWatchTarget("./ArtificialIntelligence/Tareas/Tarea-01/dist/");
+	// add my own collection, it implies return a list of objects
+	eleventyConfig.addCollection("pdfs", async (collectionAPI) => {
+		const pattern = "_site/pdfs/**/*.pdf";
+		const files = [];
+		try {
+			for await (const file of glob(pattern)) {
+				files.push(file);
+			}
+		} catch (error) {
+			throw error;
+		}
+
+		const pdfs = files.map(f => {
+			const sep = f.split(path.sep).slice(2); // remove _site, pdfs
+			const newPath = path.join(...sep); // the path inside de pdfs folder
+
+			return {
+				title: path.basename(f, ".pdf"),
+				course: sep[0],
+				url: `/pdfs/${newPath}`,
+			}
+		});
+		return pdfs;
+		
+		// return collectionAPI.getFilteredByGlob("_site/pdfs/*") doesn't work 
+		// because according to the docs it will ignore the .pdf files because .pdf is not supported by 11ty
+	});
+
+	// eleventyConfig.addTemplateFormats("js");
+	// eleventyConfig.addWatchTarget("./ArtificialIntelligence/Tareas/Tarea-01/dist/");
 
 	// "clowd" here means that the extension will apply to any .clowd file
 	// eleventyConfig.addExtension("js", {
 	// 	compile: async (inputContent) => {
-			
+
 	// 		return async () => {
 	// 			return inputContent;
 	// 		};
